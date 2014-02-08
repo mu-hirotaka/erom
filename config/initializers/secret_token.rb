6702b3f5e71eb5bdf6bbf709be4e2f242466b63d
muranaka.hirotaka <hirop4@gmail.com>
2014-02-09 03:09:05 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footiine::Application.config.secret_token = '52e42ed429b8c2b3a965e219b3fe886e5abb8f038f30d18d195d9831a4eb8a17407be4c37c962d17c89848716bea3f80a2579fc79221a9157dd782666e88015f'

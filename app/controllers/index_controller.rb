3d50a04cd283e887541c53f26bbde6a4468bac7d
muranaka.hirotaka <hirop4@gmail.com>
2014-02-19 02:11:50 +0900

    Modify index page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,6 @@
 class IndexController < ApplicationController
   def index
-    @players = Player.all
+    @round = Round.last
+    @players = Player.where(:round_id => @round.id).order('point desc')
   end
 end

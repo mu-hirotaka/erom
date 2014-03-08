3627d63c795ce737acc50e48e0f470c1d17c4c3a
muranaka.hirotaka <hirop4@gmail.com>
2014-03-09 01:28:46 +0900

    Modify article master

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require "csv"
 
-CSV.foreach('db/sample.csv') do |row|
+CSV.foreach('db/article.csv') do |row|
   Article.create(:title => row[0], :comment => row[1], :url => row[2], :total_point => 0, :today_point => 0, :visible => 1)
 end
